0c3c1d2ebee97c58a1142dae9a570d4a108c556a
Nicholas English <40871703+ikorisabi@users.noreply.github.com>
2018-07-05 19:11:58 -0400

    Create CSVHandler.php

---
+++ src/CSVHandler.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+/**
+ * A CSV handler used to manage CSV datasets in the neural network.
+ *
+ * @author @ikorisabi <ikorisabi@gmail.com>.
+ *
+ * @license <https://github.com/neyo-artificial-intelligence/csv-handler/blob/master/LICENSE> GNU General Public License v3.0.
+ *
+ * @link <https://github.com/neyo-artificial-intelligence/csv-handler> Source Code.
+ */
+
+namespace Neyo;
+
+use function getcwd;
+use function is_string;
+use function is_dir;
+use function is_int
+use function is_object;
+use function is_bool;
+use function unlink;
+
+/**
+ * @class CSVHandler.
+ */
+class CSVHandler implements CSVHandlerInterface
+{
+}
