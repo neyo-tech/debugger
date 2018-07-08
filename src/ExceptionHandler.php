a2d36dee63bc70f107982056ab33cd5b8e5e8142
ikorisabi <40871703+ikorisabi@users.noreply.github.com>
2018-07-08 15:15:10 -0400

    Create ExceptionHandler.php

---
+++ src/ExceptionHandler.php
@@ -0,0 +1,63 @@
+<?php
+declare(strict_types=1);
+/**
+ * Neyo PHP Framework.
+ *
+ * @author @ikorisabi <ikorisabi@gmail.com>.
+ *
+ * @license <https://github.com/neyo-php-framework/debugger/blob/master/LICENSE> GNU General Public License v3.0.
+ * @link    <https://github.com/neyo-php-framework/debugger>                     Source Code.
+ */
+
+namespace Neyo;
+
+use RuntimeException;
+
+use function is_null;
+use function function_exists;
+use function trim;
+
+use const null;
+
+/**
+ * @class      ExceptionHandler          The exception handler.
+ * @implements ExceptionHandlerInterface The exception handler interface.
+ */
+class ExceptionHandler implements ExceptionHandlerInterface
+{
+
+    /**
+     * @var string $passableFunction The function name.
+     */
+    private $callableFunction;
+
+    /**
+     * Initialize a new exception handler.
+     *
+     * @param string $passableFunction The function name.
+     *
+     * @throws RuntimeException If the function does not exists.
+     *
+     * @return void Returns nothing.
+     */
+    public function __construct(string $passableFunction = '')
+    {
+        $passableFunction = trim($passableFunction);
+        // An error handler function is provided.
+        if (function_exists($passableFunction)) {
+            $this->callableFunction = $passableFunction;
+        } else {
+            throw new RuntimeException('The function does not exists.');
+        }
+    }
+
+    /**
+     * Return the error handler function.
+     *
+     * @return callable The error handler function
+     */
+    public function getCallable()
+    {
+        return $callableFunction();
+    }
+}
