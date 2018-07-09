914ae9f7535d267577b8f02e5083a8ba6d2329ee
ikorisabi <ikorisabi@gmail.com>
2018-07-09 15:36:34 -0400

    Update ExceptionHandlerTest.php

--- tests/ExceptionHandlerTest.php
+++ tests/ExceptionHandlerTest.php
@@ -1,1 +1,28 @@
+<?php
+declare(strict_types=1);
+/**
+ * Neyo PHP Framework.
+ *
+ * @author @ikorisabi <ikorisabi@gmail.com>.
+ * @license <https://github.com/neyo-php-framework/debugger/blob/master/LICENSE> GNU General Public License v3.0.
+ *
+ * @link    <https://github.com/neyo-php-framework/debugger>                     Source Code.
+ */
+namespace Neyo\Tests;
 
+use Neyo\ExceptionHandler;
+
+use PHPUnit\Framework\TestCase;
+use RuntimeException;
+
+/**
+ * @class ExceptionHandlerTest The exception handler test class.
+ */
+class ErrorHandlerTest extends TestCase
+{
+    public function testConstructor()
+    {
+        $this->expectException(RuntimeException::class);
+        $exceptionHandler = new ExceptionHandler('UnkownFunction');
+    }
+}
