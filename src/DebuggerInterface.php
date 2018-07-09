0f8e5726a8c738d809f3cb121b27e5f1c9a279a8
ikorisabi <ikorisabi@gmail.com>
2018-07-09 13:19:38 -0400

    Create DebuggerInterface.php

---
+++ src/DebuggerInterface.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+/**
+ * Neyo PHP Framework.
+ *
+ * @author @ikorisabi <ikorisabi@gmail.com>.
+ *
+ * @license <https://github.com/neyo-php-framework/debugger/blob/master/LICENSE> GNU General Public License v3.0.
+ * @link    <https://github.com/neyo-php-framework/debugger>                     Source Code.
+ */
+
+namespace Neyo;
+
+/**
+ * @interface DebuggerInterface The debugger interface.
+ */
+interface DebuggerInterface
+{
+}
