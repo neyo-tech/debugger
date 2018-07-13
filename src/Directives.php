ff248175b9bb5732fbedf290dae9e5c5b71ef4fb
ikorisabi <ikorisabi@gmail.com>
2018-07-13 17:53:54 -0400

    Update Directives.php

--- src/Directives.php
+++ src/Directives.php
@@ -1,1 +1,27 @@
+<?php
+declare(strict_types=1);
+/**
+ * Neyo PHP Framework.
+ *
+ * @author @ikorisabi <ikorisabi@gmail.com>.
+ * @license <https://github.com/neyo-php-framework/debugger/blob/master/LICENSE> GNU General Public License v3.0.
+ *
+ * @link    <https://github.com/neyo-php-framework/debugger>                     Source Code.
+ */
 
+namespace Neyo;
+
+/**
+ * @class Directives A static class that defines the directives.
+ */
+class Directives
+{
+
+    /**
+     * @var array $directives The list of supported directives.
+     */
+    public static $directives = [
+        'debugger.log',
+        'debugger.session'
+    ];
+}
